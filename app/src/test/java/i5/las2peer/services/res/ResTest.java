7dc3aa515f26fd3c7e45a74c9ace1e87577614f0
GHProjectsUser <mail@rwth>
2022-07-04 09:01:43 +0000

    test

--- app/src/test/java/i5/las2peer/services/res/ResTest.java
+++ app/src/test/java/i5/las2peer/services/res/ResTest.java
@@ -116,6 +116,32 @@ public class ResTest {
   }
 
 
+  /**
+   * 
+   * Test for the SimpleGETtesttest_ID711739 method.
+   * 
+   */
+  @Test
+  public void testSimpleGETtesttest_ID711739() {
+    MiniClientCoverage c = new MiniClientCoverage(mainPath);
+    c.setConnectorEndpoint(connector.getHttpEndpoint());
+    
+        
+    try {
+      c.setLogin(AnonymousAgentImpl.IDENTIFIER, "");
+      ClientResponse result = c.sendRequest("GET", "/test", """
+""", "text/plain", "*/*", new HashMap<>(), new Object[0]);
+      System.out.println("Result of request with id: 923605: " + result.getResponse().trim());
+    
+      Assert.assertEquals("[717504]", 200, result.getHttpCode());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Exception: " + e);
+    }
+
+    
+  }
 
 
 
